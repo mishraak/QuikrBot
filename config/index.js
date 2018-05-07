9eca57c206a9aa0508cb08298c794400371e1966
Akshay Mishra <mishra.akshay91@gmail.com>
2018-05-07 16:37:40 -0700

    changed url for feedback app

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ module.exports = {
   MONGO_URL: 			'mongodb://prodowner:prodroot@ds111138.mlab.com:11138/prodfeedbackapp',	
   HASHTAG:              '#cmpe295bproblem',
   INITIAL_SINCE_ID:     987545562693054500,
-  FEEDBACK_APP_URL:     'https://cmpe295b.herokuapp.com/surveys',
+  FEEDBACK_APP_URL:     'https://cmpe295b.herokuapp.com',
   REC_ENGINE_URL:       'https://hys9p924mk.execute-api.us-west-1.amazonaws.com/v1/authorities-info', 
   REC_ENGINE_API_KEY:   'ULijX7EfaV5RRH9BwgweD5NkxfvMDV5u45rNQHoT',  
 };
