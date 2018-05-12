5ab57ebb0a6abfe8a8447e4bf9d4ec9d8b36ffca
Akshay Mishra <mishra.akshay91@gmail.com>
2018-05-12 15:23:28 -0700

    changing hashtag

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {  
   POLLING_INTERVAL:     2*1000,    
   MONGO_URL: 			'mongodb://prodowner:prodroot@ds111138.mlab.com:11138/prodfeedbackapp',	
-  HASHTAG:              '#cmpe295bproblem',
+  HASHTAG:              '#quikrbot',
   INITIAL_SINCE_ID:     987545562693054500,
   FEEDBACK_APP_URL:     'https://cmpe295b.herokuapp.com',
   REC_ENGINE_URL:       'https://hys9p924mk.execute-api.us-west-1.amazonaws.com/v1/authorities-info', 
